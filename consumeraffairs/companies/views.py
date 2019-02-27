0849df9fb7d08ba1846ff981cfc538459bdfd59a
Sqyttles <sqyttles@protonmail.com>
2019-02-27 03:24:37 -0600

    Add views via generic CBVs

--- consumeraffairs/companies/views.py
+++ consumeraffairs/companies/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, RedirectView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Company
+from .forms import CompanyForm
 
-# Create your views here.
+
+class CompanyDetailView(LoginRequiredMixin, DetailView):
+    """ Shows Details about a Company """
+    model = Company
+    slug_field = "name"
+    slug_url_kwarg = "name"
+    fields = ["name"]
+
+
+company_detail_view = CompanyDetailView.as_view()
+
+
+class CompanyListView(LoginRequiredMixin, ListView):
+    """ Lists Companies """
+    model = Company
+    slug_field = "name"
+    slug_url_kwarg = "name"
+
+
+company_list_view = CompanyListView.as_view()
+
+
+class CompanyCreateView(LoginRequiredMixin, CreateView):
+    """ Creates a Company """
+    model = Company
+    form = CompanyForm
+    fields = ['name']
+
+    def get_success_url(self):
+        return reverse_lazy("companies:list")
+
+
+company_create_view = CompanyCreateView.as_view()
+
+
+class CompanyUpdateView(LoginRequiredMixin, UpdateView):
+    """ Edits a Company """
+    model = Company
+    form = CompanyForm
+    fields = ["name"]
+    slug_field = "name"
+    slug_url_kwarg = "name"
+
+    def get_success_url(self):
+        return reverse_lazy("companies:list")
+
+
+company_update_view = CompanyUpdateView.as_view()
+
+
+class CompanyDeleteView(LoginRequiredMixin, DeleteView):
+    """ Deletes a Company """
+    model = Company
+    slug_field = "name"
+    slug_url_kwarg = "name"
+    success_url = reverse_lazy("companies:list")
+
+
+company_delete_view = CompanyDeleteView.as_view()
+
+
+class CompanyRedirectView(LoginRequiredMixin, RedirectView):
+    """ Redirects to the Company Details View """
+    permanent = False
+
+    def get_redirect_url(self):
+        return reverse_lazy("companies:detail")
+
+
+company_redirect_view = CompanyRedirectView.as_view()
--- consumeraffairs/reviews/views.py
+++ consumeraffairs/reviews/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, RedirectView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Review
+from .forms import ReviewForm
 
-# Create your views here.
+
+class ReviewDetailView(LoginRequiredMixin, DetailView):
+    """ Shows Details about a Review """
+    model = Review
+    slug_field = "title"
+    slug_url_kwarg = "title"
+    fields = ["company", "rating", "title", "summary"]
+
+
+review_detail_view = ReviewDetailView.as_view()
+
+
+class ReviewListView(LoginRequiredMixin, ListView):
+    """ Lists Reviews """
+    model = Review
+    slug_field = "title"
+    slug_url_kwarg = "title"
+
+
+review_list_view = ReviewListView.as_view()
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    """ Creates a Review """
+    model = Review
+    form = ReviewForm
+    fields = ["company", "rating", "title", "summary"]
+
+    def get_success_url(self):
+        return reverse_lazy("reviews:list")
+
+
+review_create_view = ReviewCreateView.as_view()
+
+
+class ReviewUpdateView(LoginRequiredMixin, UpdateView):
+    """ Edits a Review """
+    model = Review
+    form = ReviewForm
+    fields = ["company", "rating", "title", "summary"]
+    slug_field = "title"
+    slug_url_kwarg = "title"
+
+    def get_success_url(self):
+        return reverse_lazy("reviews:list")
+
+
+review_update_view = ReviewUpdateView.as_view()
+
+
+class ReviewDeleteView(LoginRequiredMixin, DeleteView):
+    """ Deletes a Review """
+    model = Review
+    slug_field = "title"
+    slug_url_kwarg = "title"
+    success_url = reverse_lazy("reviews:list")
+
+
+review_delete_view = ReviewDeleteView.as_view()
+
+
+class ReviewRedirectView(LoginRequiredMixin, RedirectView):
+    """ Redirects to the Review Details View """
+    permanent = False
+
+    def get_redirect_url(self):
+        return reverse_lazy("reviews:detail")
+
+
+review_redirect_view = ReviewRedirectView.as_view()
